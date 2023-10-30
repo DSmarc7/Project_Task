2a1a1826df69eba37534c36fdd63ef8b9b15ca69
marc-antoine <marc-antoine.da.silva@efrei.net>
2023-10-30 20:07:36 +0100

    Task-manager

---
+++ src/components/MainPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main-page">
+        <Header/>
+        <TaskM/>
+        <Footer/>
+    </div>
+  </template>
+  
+  <script>
+  import TaskM from './TaskM.vue';
+  import Footer from './Footer.vue';
+  import Header from './Header.vue'
+  export default {
+    name: 'MainPage',
+    components: {
+      TaskM,
+        Footer,
+        Header
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .main-page {
+    text-align: center;
+    /* padding: 10px; */
+    font-size: 1.5rem;
+    color: #555;
+  }
+  </style>
+  
